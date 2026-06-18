632d73675db510da79a7ebbd3d308a62fb064b23
JeffersonGL3132009 <geffersongomeslopes01@gmail.com>
2026-06-18 15:58:00 -0300

    Update videogames.sql

--- videogames.sql
+++ videogames.sql
@@ -6,5 +6,10 @@ nomeu varchar(50) not null,
 emailu varchar(50) not null,
 senhau varchar(20) not null);
 
+create table catalogo(
+idc int not null auto_increment primary key,
+foreign key (itemc) references videogames(idv),
+FOREIGN key (iduc) references usuario(idu));
+ 
 
  
